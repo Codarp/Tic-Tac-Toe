261b2ec530e56d08213c6df508892945a0dcc055
Arpit Gupta <arpit919gupta@gmail.com>
2017-02-26 21:18:56 +0530

    Simple game of tic-tac-toe
    Players can see their performance

---
+++ Tic_Tac_Toe/src/tic_tac_toe/Tic_Tac_Toe.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic_tac_toe;
+
+/**
+ *
+ * @author Arpit
+ */
+public class Tic_Tac_Toe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TIctac.main(new String[0]);
+    }
+    
+}
